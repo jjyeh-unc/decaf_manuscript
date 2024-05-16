bca933ebcf6e1b7443f4faa04c45eff9a05b4420
ekhar17 <ekhar@ad.unc.edu>
2024-05-15 21:26:15 -0400

    Regenerated Figures to Ensure Paths Correct

---
+++ scripts/02.decaf_training_and_validation/02.public_data_cleaning.R
@@ -0,0 +1,346 @@
+#####################################################################################
+
+##                             Data Cleaning Script                      ############
+
+####################################################################################
+
+
+
+
+######################################################################################
+############## Step 1: Create Functions for Loading Data ########################
+######################################################################################
+
+
+
+
+#### Load Raw Subtype Calls ####
+Load_data_subtype <- function(rDataName) {
+  
+ dataSet =  readRDS(sprintf("../../data/public_PDAC/%s.caf_subtype.rds", rDataName))
+  
+
+ 
+  ## Change myCAF -> permCAF & iCAF -> restCAF
+ 
+ dataSet$Subtype$SCISSORS_CAF_K2_top25.vK[dataSet$Subtype$SCISSORS_CAF_K2_top25.vK == "myCAF"] = "permCAF"
+ dataSet$Subtype$SCISSORS_CAF_K2_top25.vK[dataSet$Subtype$SCISSORS_CAF_K2_top25.vK == "Mixed.myCAF"] = "Mixed.permCAF"
+ dataSet$Subtype$SCISSORS_CAF_K2_top25.vK[dataSet$Subtype$SCISSORS_CAF_K2_top25.vK == "Mixed.iCAF"] = "Mixed.restCAF"
+  dataSet$Subtype$SCISSORS_CAF_K2_top25.vK[dataSet$Subtype$SCISSORS_CAF_K2_top25.vK == "iCAF"] = "restCAF"
+  dataSet$Subtype$CAF = dataSet$Subtype$SCISSORS_CAF_K2_top25.vK
+  dataSet$Subtype$SCISSORS_CAF_K2_top25.K2[dataSet$Subtype$SCISSORS_CAF_K2_top25.K2 == "myCAF"] = "permCAF"
+  dataSet$Subtype$SCISSORS_CAF_K2_top25.K2[dataSet$Subtype$SCISSORS_CAF_K2_top25.K2 == "iCAF"] = "restCAF"
+  
+  ## Change Mixed permCAF -> permCAF
+  dataSet$Subtype$CAF[dataSet$Subtype$CAF == "Mixed"] = NA
+  dataSet$Subtype$CAF[dataSet$Subtype$CAF == "Absent"] = NA
+  dataSet$Subtype$CAF[dataSet$Subtype$CAF == "Mixed.permCAF"] = "permCAF"
+  dataSet$Subtype$CAF[dataSet$Subtype$CAF == "Mixed.restCAF"] = "restCAF"
+  dataSet$Subtype$CAF_K4 = dataSet$Subtype$SCISSORS_CAF_K2_top25.vK
+  
+  ## remove Absent Call 
+  dataSet$Subtype$CAF_ALL = dataSet$Subtype$CAF_K4
+  
+  dataSet$Subtype$CAF_K4[dataSet$Subtype$CAF_K4 == "Absent"] = NA
+  
+  dataSet$Subtype$CAF.classifier = dataSet$Subtype$SCISSORS_CAF_K2_top25.vK.classifier
+  
+  return(dataSet)
+}
+
+
+########################################
+
+
+#### Load Raw Survival Data ####
+Load_data_survival <- function(rDataName) {
+  
+ dataSet =  readRDS(sprintf("../../data/public_PDAC/%s.survival_data.rds", rDataName))
+  
+  
+  return(dataSet)
+}
+
+
+
+
+##################################
+
+#### Load Raw Gene and Clinical Data ####
+Load_data_gene <- function(rDataName, type = c("array", "seq")) {
+  
+  
+  dat = try(readRDS(sprintf("../../data/public_PDAC/%s.rds", rDataName)))
+  
+  # process expression information
+  dat2 = dat$ex
+  dat2 = dat2[!is.na(dat$featInfo$SYMBOL),]
+  genes = dat$featInfo$SYMBOL[!is.na(dat$featInfo$SYMBOL)]
+  if(any(table(genes) > 1)){
+    # need to collapse expression
+    if(type == "array"){
+      dat2 = aggregate(. ~ genes, dat = dat2, FUN = mean)
+      rownames(dat2) = dat2[,1]
+      dat2 = dat2[,-1]
+    }else{
+      dat2 = aggregate(. ~ genes, dat = data.frame(dat2), FUN = sum)
+      rownames(dat2) = dat2[,1]
+      dat2 = dat2[,-1]
+      #dat2 = dat2[rowMeans(dat2 > 5) > .2 ,]
+    }
+  }else{
+    rownames(dat2) = dat$featInfo$SYMBOL
+  }
+  
+  colnames(dat2) = colnames(dat$ex)     
+  
+  ## Resave back to dat
+  dat$expression = dat2
+  
+  
+  return(dat)
+}
+
+
+
+#################################
+
+
+## Generate Cleaned Survival data
+Generate_Survival_Data <- function(survSet, subtypeSet, DeCAF_calls, save, rDataName) {
+  
+  survDat <- data.frame(ID = survSet$sampID,
+                        time = as.numeric(survSet$time),
+                        status = as.numeric(survSet$event),
+                        surv_whitelist = survSet$whitelist,
+                        SCISSORS_CAF = subtypeSet$Subtype$CAF,
+                        SCISSORS_CAF_K4 = subtypeSet$Subtype$CAF_K4,
+                        SCISSORS_CAF_ALL = subtypeSet$Subtype$CAF_ALL,
+                        SCISSORS_CAF_K2 = subtypeSet$Subtype$SCISSORS_CAF_K2_top25.K2,
+                        MS_K2 = subtypeSet$Subtype$MS,
+                        Elyada_CAF = subtypeSet$Subtype$Elyada_CAF,
+                        PurIST = subtypeSet$Subtype$PurIST,
+                        Maurer =  subtypeSet$Subtype$Maurer,
+                        Puleo = subtypeSet$Subtype$Puleo,
+                        DeCAF = DeCAF_calls$DeCAF,
+                        DeCAF_prob = DeCAF_calls$DeCAF_prob,
+                        stringsAsFactors = FALSE)
+  survDat <- survDat[survSet$whitelist == TRUE,]
+  
+  if(save == TRUE){
+    filename = paste("../../data/public_PDAC_clean/survival/", rDataName, "_survival.csv", sep = "")
+    write.csv(survDat,filename, row.names = FALSE)
+  }
+  return(survDat)
+}
+
+#################################
+
+
+## Generate Cleaned Survival data
+Generate_SuppTable_Data <- function(survSet, subtypeSet, DeCAF_calls, rDataName) {
+  
+  SuppTable_Data <- data.frame(Dataset = rDataName,
+                               ID = subtypeSet$Subtype$sampID,
+                               PDAC_Pi_whitelist = !is.na(subtypeSet$Subtype$CAF_ALL),
+                               DeCAF = DeCAF_calls$DeCAF,
+                               DeCAF_graded = DeCAF_calls$DeCAF_graded,
+                               DeCAF_prob = DeCAF_calls$DeCAF_prob,
+                               PurIST = subtypeSet$Subtype$PurIST,
+                               PurIST_graded = subtypeSet$Subtype$PurIST_graded,
+                               PurIST_prob = subtypeSet$Subtype$PurIST.prob,
+                               SCISSORS_K2 = subtypeSet$Subtype$SCISSORS_CAF_K2_top25.K2,
+                               SCISSORS_vK = subtypeSet$Subtype$SCISSORS_CAF_K2_top25.vK,
+                               SCISSORS_combined = subtypeSet$Subtype$CAF,
+                               SCISSORS_labels_whitelist = !is.na(subtypeSet$Subtype$CAF) & subtypeSet$Subtype$SCISSORS_CAF_K2_top25.vK.classifier,
+                               Elyada = subtypeSet$Subtype$Elyada_CAF,
+                               Moffitt_stroma = subtypeSet$Subtype$MS,
+                               Maurer =  subtypeSet$Subtype$Maurer,
+                               Puleo = subtypeSet$Subtype$Puleo)
+  if(class(survSet) != "data.frame"){
+    SuppTable_Data$Survival_analysis_whitelist = F
+    SuppTable_Data$OS_time = NA
+    SuppTable_Data$OS_event = NA
+  }
+  if(class(survSet) == "data.frame"){
+    SuppTable_Data$Survival_analysis_whitelist = survSet$whitelist
+    SuppTable_Data$OS_time = as.numeric(survSet$time)
+    SuppTable_Data$OS_event = as.numeric(survSet$event)
+  }
+
+
+  return(SuppTable_Data)
+}
+
+## Generate Cleaned Subtype data
+Generate_Subtype_Data <- function(subtypeSet, DeCAF_calls, save, rDataName) {
+  
+  subtype <- data.frame(ID = subtypeSet$Subtype$sampID,
+                        SCISSORS_CAF = subtypeSet$Subtype$CAF,
+                        SCISSORS_CAF_K4 = subtypeSet$Subtype$CAF_K4,
+                        SCISSORS_CAF_ALL = subtypeSet$Subtype$CAF_ALL,
+                        SCISSORS_CAF_K2 = subtypeSet$Subtype$SCISSORS_CAF_K2_top25.K2,
+                        MS_K2 = subtypeSet$Subtype$MS,
+                        Elyada_CAF = subtypeSet$Subtype$Elyada_CAF,
+                        PurIST = subtypeSet$Subtype$PurIST,
+                        Maurer =  subtypeSet$Subtype$Maurer,
+                        Puleo = subtypeSet$Subtype$Puleo,
+                        DeCAF = DeCAF_calls$DeCAF,
+                        CAF_whitelist = subtypeSet$Subtype$CAF.classifier,
+                        stringsAsFactors = FALSE)
+  subtype <- subtype[subtype$CAF_whitelist == TRUE,]
+  
+  if(save == TRUE){
+    filename = paste("../../data/public_PDAC_clean/subtype/", rDataName, "_subtype.csv", sep = "")
+    write.csv(subtype,filename, row.names = FALSE)
+  }
+  return(subtype)
+}
+
+
+################################
+
+
+
+## Extract Classifier Genes
+Extract_Classifier_Genes <- function(data, classifier, subtypeSet, save, rDataName) {
+ 
+  rows = which(rownames(data) %in% classifier$TSPs[classifier$fit$beta[-1] != 0,])
+  white_list = subtypeSet$Subtype$CAF.classifier
+  if(sum(is.na(white_list)) == nrow(subtypeSet$Subtype)){
+    white_list = rep(T, nrow(subtypeSet$Subtype))
+  }
+  data = data[rows,white_list]
+  tsp = match(as.vector(t(classifier$TSPs[classifier$fit$beta[-1]!=0,])),rownames(data))
+  data = data[tsp,]
+  
+  
+  if(save == TRUE){
+    filename = paste("../../data/public_PDAC_clean/classifier_gene_exp/", rDataName, "_classifier_gene_expression.csv", sep = "")
+    write.csv(data,filename, row.names = FALSE)
+  }
+  return(data)
+}
+
+
+
+######################################################################################
+################ Step 2: Load Classifier #############################################
+######################################################################################
+
+## Set working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+## Load Classifier
+load("../R/DeCAF/decaf_classifier.Rdata")
+classifier = decaf_classifier$classifier2
+
+## Load Evaluate Classifier Function 
+source("../R/DeCAF/decaf_functions.R")
+training = unique(decaf_classifier$anno$study)
+
+
+
+
+######################################################################################
+######## Step 3: Clean Survival, Subtype, and Clinical Raw Data #######################
+######################################################################################
+
+
+
+
+## Names of Data
+DataNames <- c("CPTAC", "Dijk", "Grunwald", "Hayashi",
+               "Linehan", "Moffitt_GEO_array", "Olive", "PACA_AU_array", "PACA_AU_seq",
+               "Puleo_array", "TCGA_PAAD")
+## Data Types
+data_type = c("seq", "seq", "seq", "seq",
+              "seq", "array", "seq", "array", "seq",
+              "array", "seq")
+## Is there survival info?
+survival_info = c(T, T, T, F,
+              T, T, F, T, T,
+              T, T)
+
+
+
+## Loop Through All Data
+for(i in 1:length(DataNames)){
+  
+  print(paste(DataNames[i], data_type[i], "survival info", survival_info[i], sep = " - "))
+  
+  ## Load Data
+  rDataName <- DataNames[i]
+  subtypeSet <- Load_data_subtype(rDataName)
+  dataSet <- Load_data_gene(rDataName, type = data_type[i] )
+  print("loading done")
+  
+  ## Generate DeCAF Calls
+  DeCAF_calls <- apply_decaf(dataSet$expression, classifier)
+  
+  ## Clean and Save Survival Data if applicable
+  if(survival_info[i]){
+    survSet <- Load_data_survival(rDataName)
+    survDat <- Generate_Survival_Data(survSet, subtypeSet, DeCAF_calls, save = TRUE, rDataName)
+    survDat$dataSet <- rDataName
+    if(i == 1){
+      survDatCmb <- survDat
+    } else {
+      survDatCmb <- rbind(survDatCmb,survDat)
+    }
+    
+  }
+  if(!survival_info[i]){
+    survSet = NA
+  }
+  
+  ## Save for Supplemental Table
+  SuppTable_Data_study = Generate_SuppTable_Data(survSet, subtypeSet, DeCAF_calls, rDataName)
+  
+  ## Clean and Save Data
+  subtypeDat <- Generate_Subtype_Data(subtypeSet, DeCAF_calls, save = TRUE, rDataName)
+  classif_exp <- Extract_Classifier_Genes(dataSet$expression, classifier, subtypeSet, save = T, rDataName)
+  
+  
+  ## Add Study Name
+  subtypeDat$dataSet <- rDataName
+  DeCAF_calls$dataSet <- rDataName
+  
+  ## Only Subtype True DeCAF Calls
+  DeCAF_calls = DeCAF_calls[subtypeSet$Subtype$CAF.classifier,]
+  
+  ## Combine With Other Studies
+  if(i == 1){
+    subtypeDatCmb <- subtypeDat
+    classif_expCmb <- classif_exp
+    DeCAF_callsCmb <- DeCAF_calls
+    SuppTable_Data <- SuppTable_Data_study
+  } else {
+    subtypeDatCmb <- rbind(subtypeDatCmb, subtypeDat)
+    classif_expCmb <- cbind(classif_expCmb, classif_exp)
+    DeCAF_callsCmb <- rbind(DeCAF_callsCmb, DeCAF_calls)
+    SuppTable_Data <- rbind(SuppTable_Data, SuppTable_Data_study)
+  }
+  
+  
+  
+  
+}
+
+
+
+
+## Save Combined Data
+write.csv(survDatCmb,"../../data/public_PDAC_clean/combined_data/combined_survival_DeCAF.csv", row.names = FALSE)
+write.csv(subtypeDatCmb,"../../data/public_PDAC_clean/combined_data/combined_subtype_DeCAF.csv", row.names = FALSE)
+write.csv(classif_expCmb,"../../data/public_PDAC_clean/combined_data/combined_classifier_gene_exp_DeCAF.csv")
+write.csv(DeCAF_callsCmb,"../../data/public_PDAC_clean/combined_data/combined_classifier_calls_DeCAF.csv")
+
+## Change Survival of Duplicated ID's to Individual Level Only
+idDup <- SuppTable_Data$ID[(duplicated(SuppTable_Data$ID))]
+idDup_Paca_array <- which((SuppTable_Data$ID %in% idDup) & (SuppTable_Data$dataSet == "PACA_AU_array"))
+SuppTable_Data$Survival_analysis_whitelist[idDup_Paca_array] <- "Individual Study Only - Not Combined"
+write.xlsx(SuppTable_Data, "../../results/tables/Supplementary Table - clinical and molecular data - public.xlsx", row.names = F)
+
+
+
---
+++ scripts/02.decaf_training_and_validation/03.generate_classifier_eval_figures.R
@@ -0,0 +1,302 @@
+#####################################################################################
+
+##########     Classifier Evaluation Figure Generating Script             ############
+
+####################################################################################
+
+
+
+
+######################################################################################
+############## Step 1: Create Functions for Generating Figures ########################
+######################################################################################
+
+
+
+
+# load libraries
+library(pheatmap)
+library(ggplot2)
+library(scales)
+library(caret)
+library(pROC)
+library(kableExtra)
+library(nsROC)
+library(ROCR)
+
+## Colors for DeCAF
+DeCAFList = c("permCAF","restCAF")
+DeCAFCol = c("violetred1","turquoise4")
+
+############################################################
+
+## Function for Heatmap
+Plot_heatmap_function <- function(subtype_data, gene_data, DeCAF_data, classifier, training, CAF_type = c("K2", "K4"), schemaList, include_PurIST = F){
+  
+  ## List of Samples To Include
+  if(CAF_type == "K4"){
+    samples_subset = !(subtype_data$dataSet %in% training) & !(is.na(subtype_data$SCISSORS_CAF_ALL))
+  }
+  
+  if(CAF_type == "K2"){
+    samples_subset = !(subtype_data$dataSet %in% training) & !(is.na(subtype_data$SCISSORS_CAF))
+  }
+  
+  
+  ## Transform TSP's to ranks
+  TSPgeneMat = apply(gene_data[,samples_subset], 2, rank)
+  
+  ## Transfrom Calls to Ordered Factor
+  DeCAF_data$DeCAF_graded = factor(DeCAF_data$DeCAF_graded, levels = c("Strong restCAF", "Likely restCAF", "Lean restCAF",
+                                                                                           "Lean permCAF", "Likely permCAF", "Strong permCAF"))
+  ## Make ColorScapr
+  colscale = colorRampPalette(c("turquoise4","white", "violetred1"))(6)
+  colscale2 = colorRampPalette(c("turquoise4","white", "violetred1"))(5)
+  colscale3 = colorRampPalette(c("turquoise4","white", "violetred1"))(2)
+  colscale4 = c(colscale2, "black")
+  
+  # Create cleaned up study labels
+  Study = subtype_data$dataSet[samples_subset]
+  
+  
+  
+  ## Change CAF based on if K2 or K4 required
+  if(CAF_type == "K4"){
+    cc_subtype = subtype_data$SCISSORS_CAF_ALL[samples_subset]
+    cc_subtype_names = c("restCAF", "Mixed.restCAF","Mixed","Mixed.permCAF", "permCAF", "Absent")
+    cc_colscale = colscale4
+  }
+  if(CAF_type == "K2"){
+    cc_subtype = subtype_data$SCISSORS_CAF[samples_subset]
+    cc_subtype_names = c("restCAF", "permCAF")
+    cc_colscale = colscale3
+  }
+  
+  
+  # create annotation vector for the samples
+  annotation_col = data.frame(Study = Study, DeCAF = DeCAF_data$DeCAF_graded[samples_subset],
+                              'CC Subtype' = cc_subtype,
+                              # "Elyada CAF" = subtype_data$Elyada_CAF[samples_subset],
+                              #"Moffitt Stroma" = subtype_data$MS_K2[samples_subset], 
+                              check.names = F)
+  if(include_PurIST == T){
+    annotation_col$PurIST = subtype_data$PurIST[samples_subset]
+  }
+  rownames(annotation_col) = colnames(gene_data)[samples_subset]
+  
+  # create annotation color vector
+
+  index_PurIST = which(schemaList == "PurIST")
+  annotation_col_colors = list(
+    DeCAF = colscale,
+    'CC Subtype' = cc_colscale,
+    'Training Labels' = DeCAFCol,
+    'Up Subtype' =DeCAFCol
+  )
+  names(annotation_col_colors$'CC Subtype')  = cc_subtype_names
+  names(annotation_col_colors$'Training Labels') = DeCAFList
+  names(annotation_col_colors$DeCAF) = levels(DeCAF_data$DeCAF_graded)
+  names(annotation_col_colors$'Up Subtype') = DeCAFList
+  
+  if(include_PurIST == T){
+    annotation_col_colors$PurIST = subtypeColList[[index_PurIST]]
+    names(annotation_col_colors$PurIST) = subtypeList[[index_PurIST]]
+  }
+  
+  
+  
+  ## Sort columns and rows of heatmap
+  sort = order(DeCAF_data$DeCAF_prob[samples_subset])
+  tsp = match(as.vector(t(classifier$TSPs[classifier$fit$beta[-1]!=0,])),rownames(TSPgeneMat))
+  
+  ## Classifier gene annotation
+  annotation_row = data.frame('Up Subtype' = factor((rep(DeCAFList  ,length(tsp)/2))),check.names = F)
+  rownames(annotation_row) = rownames(TSPgeneMat)[tsp]
+  
+  
+  ## Generate heatmap
+  heatmap = pheatmap(TSPgeneMat[tsp,sort], annotation_col = annotation_col[sort,],annotation_row = annotation_row, 
+                     annotation_colors = annotation_col_colors,show_colnames = F, cluster_cols = F,cluster_rows = F,
+                     annotation_names_row = F,width = 12, height = 12,fontsize = 11)
+  
+  
+  
+  ## Output
+  return(heatmap)
+  
+  
+}
+
+###################################################################################################
+
+## Function to Generate Pooled ROC
+Plot_Pooled_ROC <- function(subtype_data, DeCAF_data,  training){
+  
+  ## Testing Set
+  testing0 =  unique(subtype_data$dataSet)[!unique(subtype_data$dataSet) %in% training]
+  
+  ## Sample Subset
+  samples_subset = !(subtype_data$dataSet %in% training) & !is.na(subtype_data$SCISSORS_CAF)
+  DeCAF_data = DeCAF_data[samples_subset,]
+  subtype_data = subtype_data[samples_subset,]
+  
+  ## Calculate TP, FP, TN, FN
+  data = NULL
+  for(i in 1:(length(testing0))){
+    pred = DeCAF_data$DeCAF[DeCAF_data$dataSet == testing0[i]]
+    pred_p = DeCAF_data$DeCAF_prob[DeCAF_data$dataSet == testing0[i]]
+    label = subtype_data$SCISSORS_CAF[subtype_data$dataSet == testing0[i]]
+    pred = prediction(prediction = pred_p, labels = as.numeric(label == "permCAF"))
+    TP = as.numeric(attr(pred,"tp")[[1]])
+    FP = as.numeric(attr(pred,"fp")[[1]])
+    TN = as.numeric(attr(pred,"tn")[[1]])
+    FN = as.numeric(attr(pred,"fn")[[1]])
+    Author = rep(i, length(TP))
+    data = rbind(data, cbind(Author, TP, FP, TN, FN))
+  }
+  data  = data.frame(data)
+  
+  
+  
+  
+  
+    ## Make ROC curve
+    return(
+      metaROC(data,cex.main = 1,cex.lab =1,cex.axis = 1, model="random-effects", plot.Author=TRUE,plot.inter.var = T,cex.Author = .5,lwd.Author = 2,alpha.trans = .25,Ni = 100)
+      
+    )  
+  
+  
+  
+}
+  
+
+
+#####################################################################################################
+
+
+## Function to Generate Table of Classifier Performance
+Table_Classif_Perform <- function(subtype_data, DeCAF_data,  training){
+  
+  ## Testing Set
+  testing0 =  unique(subtype_data$dataSet)[!unique(subtype_data$dataSet) %in% training]
+  
+  ## Sample Subset
+  samples_subset = !(subtype_data$dataSet %in% training) & !is.na(subtype_data$SCISSORS_CAF)
+  DeCAF_data = DeCAF_data[samples_subset,]
+  subtype_data = subtype_data[samples_subset,]
+  
+  ## Empty Vector for metrics
+  N = c()
+  permCAF = c()
+  Accuracy = c()
+  Sensitivity = c()
+  Specificity = c()
+  AUC = c()
+  
+  
+  samples_subset_train = (SubtypeCmb$dataSet %in% training) & !is.na(SubtypeCmb$SCISSORS_CAF)
+  DeCAF_data_t = DeCAFCmb[samples_subset_train,]
+  subtype_data_t = SubtypeCmb[samples_subset_train,]
+  
+  
+  ## Obtain Metric for each study 
+  for(i in 1:(length(testing0))){
+    pred = DeCAF_data$DeCAF[DeCAF_data$dataSet == testing0[i]]
+    pred_p = DeCAF_data$DeCAF_prob[DeCAF_data$dataSet == testing0[i]]
+    label = subtype_data$SCISSORS_CAF[subtype_data$dataSet == testing0[i]]
+    N[i] = length(label)
+    permCAF[i] = sum(label == "permCAF")
+    Accuracy[i] = round(mean(pred == label),3)
+    Sensitivity[i] = round(sensitivity(as.factor(pred), as.factor(label), positive = "permCAF"),3)
+    Specificity[i] = round(specificity(as.factor(pred), as.factor(label), negative = "restCAF"),3)
+    roc = roc(response = label, predictor = pred_p, levels = c("restCAF", "permCAF"), quiet = T)
+    AUC[i] = round(roc$auc[1],3)
+    
+    
+  }
+  
+  ## Combien Results into one df
+  pmat = data.frame(N, permCAF, Accuracy, Sensitivity, Specificity, AUC)
+  
+  ## Clean up Study Name
+  testing0 = gsub("_PrimaryPDAC","",testing0)
+  rownames(pmat) = testing0[1:(length(testing0))]
+  
+  
+  
+  return(pmat)
+  
+
+  
+}
+
+
+
+  
+######################################################################################
+##############          Step 2: Load In Data                 ########################
+######################################################################################
+
+## Set working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+## Load Subtype, Gene Data, and DeCAF Calls
+SubtypeCmb <- read.csv("../../data/public_PDAC_clean/combined_data/combined_subtype_DeCAF.csv")
+GeneDataCmb <- read.csv("../../data/public_PDAC_clean/combined_data/combined_classifier_gene_exp_DeCAF.csv",
+                        row.names = 1)
+DeCAFCmb <- read.csv("../../data/public_PDAC_clean/combined_data/combined_classifier_calls_DeCAF.csv")
+
+## Load Classifier to obtain training groups
+load("../R/DeCAF/decaf_classifier.Rdata")
+classifier = decaf_classifier$classifier2
+training = unique(decaf_classifier$anno$study)
+
+## Load Schema List - Aka Color Codes for other classifiers
+load("../../data/cmbSubtypes.RData")
+
+
+
+######################################################################################
+##############          Step 3: Generate Figures              ########################
+######################################################################################
+
+#CAF K4 Figure Generator
+K4_heatmap = Plot_heatmap_function(subtype_data = SubtypeCmb, 
+                                   gene_data = GeneDataCmb, 
+                                   DeCAF_data = DeCAFCmb, 
+                                   classifier = classifier, 
+                                   training = training, 
+                                   CAF_type = "K4",
+                                   schemaList = schemaList, include_PurIST = T)
+
+filename =  paste("../../results/figures/heatmap_DeCAF_v_SCISSORS.pdf", sep ="")
+pdf(filename, width = 10, height = 7)
+K4_heatmap
+dev.off()
+
+
+## Figure 4E Generator
+ROC_curve = Plot_Pooled_ROC(subtype_data = SubtypeCmb, DeCAF_data = DeCAFCmb,  training = training)
+filename =  paste("../../results/figures/DeCAF_meta_ROC.pdf", sep ="")
+
+pdf(filename, height = 7, width = 7, pointsize = 25)
+Plot_Pooled_ROC(subtype_data = SubtypeCmb, DeCAF_data = DeCAFCmb,  training = training)
+dev.off()
+dev.off()
+
+
+## Figure 4G Generator
+table = Table_Classif_Perform(subtype_data = SubtypeCmb, DeCAF_data = DeCAFCmb,  training = training)
+write.csv(table, "../../results/tables/DeCAF_Metrics_ByStudy.csv")
+
+
+
+
+
+
+
+
+
+
+
